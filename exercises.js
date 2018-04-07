d09c03e809b3d05d272f96f9034e3a74794a0362
Erin Furuichi <erinf@Erins-MacBook-Pro.local>
2018-04-06 21:59:30 -1000

    logic4

--- exercises.js
+++ exercises.js
@@ -89,8 +89,14 @@ console.log(isGreaterThan(100, 50));
  * The function will return true if the value passed into the function is "true".
  * Console.log your result.
 */
-
-
+function mustBeTrue(boo){
+  if (boo==="true"){
+    return "true";
+  }else{
+    return "false";
+  }
+}
+console.log(mustBeTrue("true"));
 
 /*
  * #5
