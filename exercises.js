6a021388d186d56efa9a6ca1467215dada8763ea
Erin Furuichi <erinf@Erins-MacBook-Pro.local>
2018-04-09 18:11:27 -1000

    logicFB16

--- exercises.js
+++ exercises.js
@@ -382,7 +382,10 @@ for (var i = 0; i<5; i++){
  * #16
  * Create a for loop that will iterate and console.log each item in the array below:
 */
-
+var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
+for (var i = 0; i<myFavFoods.length; i++){
+  console.log(myFavFoods[i]);
+}
 /*
  * #17
  * Function - sumItUp
